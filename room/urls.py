cf54d59912fad4a62f8120509ece50252b4e4203
Arox <mailofarox@gmail.com>
2017-08-14 10:34:29 +0300

    add view old messages

--- room/urls.py
+++ room/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.WindowView.as_view(), name='main'),
     url(r'^add/$', views.RoomView.as_view(), name='create_room'),
     url(r'^users/(?P<pk>[0-9\-]+)/$', views.RoomUsersView.as_view(), name='users_room'),
+    url(r'^messages/(?P<pk>[0-9\-]+)/$', views.RoomMessage.as_view(), name='messages_room'),
 ]
--- room/views.py
+++ room/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 import json
 
-from .models import Room
+from .models import Room, ChatMessage
 from my_auth.models import User
 
 # Create your views here.
@@ -38,5 +38,16 @@ class RoomUsersView(View):
             room = Room.objects.get(pk=kwargs['pk'])
             users = User.objects.filter(pk__in=[x.pk for x in room.users.all()])
             result = ['{} {}'.format(x.first_name, x.last_name) for x in users]
+            return HttpResponse(json.dumps(result), status=200, content_type="application/json")
+        return HttpResponse(status=400)
+
+
+class RoomMessage(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated and isinstance(request.user, User):
+            room = Room.objects.get(pk=kwargs['pk'])
+            messages = ChatMessage.objects.filter(room=room).order_by('time')
+            result = [{'time': str(x.time), 'user': '{} {}'.format(x.author.first_name, x.author.last_name), 'message': x.text} for x in messages]
+            print(result)
             return HttpResponse(json.dumps(result), status=200, content_type="application/json")
         return HttpResponse(status=400)
